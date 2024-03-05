d7e1ff5050b396bc864a5f45602621555f8b557b
Nikola <djuricicnikola@outlook.com>
2024-03-05 16:21:41 +0100

    Changes on Web Config

--- src/main/java/com/example/translateApp/Security/WebConfig.java
+++ src/main/java/com/example/translateApp/Security/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,9 +18,7 @@ public class WebConfig {
                         .requestMatchers("/hello-rest").permitAll()
                         .requestMatchers("/hello").permitAll()
                         .requestMatchers("/translateHello").permitAll())
-                .csrf((csrf) -> csrf.ignoringRequestMatchers("/secure/hello")
-                        .ignoringRequestMatchers("/secure/admin")
-                        .ignoringRequestMatchers("/translator"))
+                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults());
 
         return httpSecurity.build();
